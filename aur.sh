2475e89ba4d03533df3a1c3819a714f493138162
Reza Farrahi M <imriss@yahoo.com>
2017-04-05 08:44:19 -0400

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -3,6 +3,7 @@ d=${BUILDDIR:-$PWD}
 for p in ${@##-*}
 do
 cd "$d"
+echo "https://aur.archlinux.org/packages/${p:0:2}/$p/$p.tar.gz" 
 curl "https://aur.archlinux.org/packages/${p:0:2}/$p/$p.tar.gz" |tar xz
 cd "$p"
 makepkg -V --skippgpcheck ${@##[^\-]*}
