aa390f1b01258933584192bd259ce60ad9d6e276
Reza Farrahi M <imriss@yahoo.com>
2017-04-06 10:46:20 -0400

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -7,10 +7,10 @@ cd "$d"
 echo "https://aur.archlinux.org/cgit/aur.git/snapshot/$p.tar.gz" 
 curl "https://aur.archlinux.org/cgit/aur.git/snapshot/$p.tar.gz" |tar xz
 cd "$p"
-echo "makepkg -V --skippgpcheck ${@##[^\-]*}"
+echo "makepkg --skippgpcheck ${@##[^\-]*}"
 makepkg --skippgpcheck ${@##[^\-]*}
 echo "$d"
 /usr/sbin/find / -name "$p.*" 
-ls -laR "$d"
+# ls -laR "$d"
 # su root -c 'pacman -U $p.tar.xz'
 done
